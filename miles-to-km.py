3427d932d451760e863833f989163e128a7b90c2
HARISH6000 <harishgovind6000@gmail.com>
2021-09-05 20:32:11 +0530

    debugged: now the converter accepts floats also

--- miles-to-km.py
+++ miles-to-km.py
@@ -2,7 +2,7 @@
 
 def function(miles):
     return 1.6*miles
-m =int(input("Enter the distance in miles:"))
+m = float(input("Enter the distance in miles:"))
 km = function(m)
 
 print(m,"miles is equal to",km,"km")
