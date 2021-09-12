a0af65b391e2255d69d7f86967716f9f3f0614e8
HARISH6000 <harishgovind6000@gmail.com>
2021-09-12 20:59:45 +0530

    it calculates the volume of cone

---
+++ Volume-of-cone.py
@@ -0,0 +1,10 @@
+def volume(r ,h):
+    v = (1/3)*((3.14)*(r*r)*(h))
+    print("the volume of the cone is ", v)
+
+a= float(input("Enter the radius:"))
+b = float(input("Enter the height:"))
+
+volume(a, b)
+
+
